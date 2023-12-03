a2d2986c0a0dd90351c954e4b0e6439ba9d99e9c
jupiter <ss0922071773@gmail.com>
2023-12-03 13:34:05 +0800

    test

---
+++ main.c
@@ -0,0 +1,32 @@
+﻿#include "stdlib.h"
+#include "stdio.h"
+#include "conio.h"
+
+int main(int argc,char argv[]){
+    system("chcp 65001");
+    system("cls");
+    printf("歡迎使用簡易記帳簿\n");
+    while(1){
+        printf("請選擇功能: (1)鍵入紀錄 (2)查看紀錄 (3)刪除紀錄 (4)離開應用");
+        switch (getch()){
+        case '1':
+            /* code */
+            break;
+        case '2':
+            /* code */
+            break;
+        case '3':
+            /* code */
+            break;
+        case '4':
+            /* code */
+            break;
+        default:
+            system("cls");
+            printf("本系統無此功能，請選擇正確的功能\n");
+            break;
+        }
+    }
+    system("pause");
+    return 0;
+}
