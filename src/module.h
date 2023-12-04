f9eb8f2e3613320feeea91a411c6191a07038094
jupiter <ss0922071773@gmail.com>
2023-12-04 12:47:57 +0800

    .

---
+++ src/module.h
@@ -0,0 +1,4 @@
+#ifndef _T_MODULE_H
+#define _T_MODULE_H
+void addRecord(void);
+#endif
---
+++ src/showInfo.h
@@ -0,0 +1,5 @@
+#ifndef SHOWINFO_H
+#define SHOWINFO_H
+void showIndex(char *lock,char **text);
+void showDate(char *date);
+#endif 
